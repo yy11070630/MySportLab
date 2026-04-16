2b1757d26a795371863c6c42ff83bafbc99dd2bf
Lim Shao Qi <shaoqilim123@gmail.com>
2026-04-16 22:31:45 +0800

    fvwefv

--- app.py
+++ app.py
@@ -122,4 +122,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         print('Database created!')
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True, port=5000)
