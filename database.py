c824b4f5ab59feb9b58349c048135e7d84e87994
Aloysius Chuah Ming Yi <aloysiuschuah@gmail.com>
2026-04-27 13:05:36 +0800

    update

--- database.py
+++ database.py
@@ -45,7 +45,8 @@ class Admin(db.Model):
     __tablename__ = 'admin'
     
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False) #hhh
+
     password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
