0c5613d156d6cdedb8fc9ffd1b6554f18d6d934c
Aloysius Chuah Ming Yi <aloysiuschuah@gmail.com>
2026-04-27 13:00:06 +0800

    admindash

--- database.py
+++ database.py
@@ -40,3 +40,12 @@ class UserProfile(db.Model):
     bio = db.Column(db.Text, nullable=True)
     date_of_birth = db.Column(db.Date, nullable=True)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+class Admin(db.Model):
+    __tablename__ = 'admin'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
